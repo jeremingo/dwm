19492f8360ea001ea546302ceec2495c0a125cab
jeremingo <58335558+jeremingo@users.noreply.github.com>
2022-05-23 17:11:39 +0300

    change colors to purple gruvbox

--- config.def.h
+++ config.def.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack:size=10" };
 static const char dmenufont[]       = "Hack:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char gruvbox_bg0[]       = "#282828";
+static const char gruvbox_bg1[]       = "#3c3836";
+static const char gruvbox_fg3[]       = "#bdae93";
+static const char gruvbox_fg1[]       = "#ebdbb2";
+static const char gruvbox_purple[]        = "#d3869b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { gruvbox_fg3, gruvbox_bg0, gruvbox_bg1 },
+	[SchemeSel]  = { gruvbox_fg1, gruvbox_purple,  gruvbox_purple  },
 };
 
 /* tagging */
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", gruvbox_bg0, "-nf", gruvbox_fg3, "-sb", gruvbox_purple, "-sf", gruvbox_fg1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
