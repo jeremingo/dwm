d5b77caca38fe0ed4434e1bee179c4e0650d931e
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-10-28 16:37:56 +0200

    Revert "Remove dmenumon variable"
    This reverts commit c2b748e7931e5f28984efc236f9b1a212dbc65e8.
    
    Revert back this change. It seems to not be an edge-case anymore since
    multiple users have asked about this new behaviour now.

--- config.def.h
+++ config.def.h
@@ -56,7 +56,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", gruvbox_bg0, "-nf", gruvbox_fg3, "-sb", gruvbox_purple, "-sf", gruvbox_fg1, NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", gruvbox_bg0, "-nf", gruvbox_fg3, "-sb", gruvbox_purple, "-sf", gruvbox_fg1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
--- dwm.c
+++ dwm.c
@@ -1647,6 +1647,8 @@ showhide(Client *c)
 void
 spawn(const Arg *arg)
 {
+	if (arg->v == dmenucmd)
+		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
