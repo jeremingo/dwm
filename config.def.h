b4bafdbe0c553755dc64e7ecc026a9e18897c9de
jeremingo <58335558+jeremingo@users.noreply.github.com>
2024-04-23 13:43:09 +0300

    swap monocle and floating layouts

--- config.def.h
+++ config.def.h
@@ -43,8 +43,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "󰙀",      tile },    /* first entry is default */
-	{ "󰗣",      NULL },    /* no layout function means floating behavior */
 	{ "󰎡",      monocle },
+	{ "󰗣",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -78,8 +78,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
