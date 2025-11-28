2a7d7c7c0014074432d47d09927074792fd7cda2
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-11-28 18:50:16 +0200

    Add dm-search Script

--- config.def.h
+++ config.def.h
@@ -91,6 +91,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_n,      spawn,          SHCMD("dm-network") },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("dm-power") },
 	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("dm-man") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("dm-search") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
